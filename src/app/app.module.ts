4dfda983e628b29045279e7ce3f4f982ee442ae7
Urvish Sojitra <urvish.sojitra@seaflux.tech>
2022-06-22 13:21:58 +0530

    feat: creating components using CLI and manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
